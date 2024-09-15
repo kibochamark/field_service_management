5e6741ef84bd133d4f4a09fac342ae01b64f021e
kibochamark <mark.varu@student.cuk.ac.ke>
2024-09-15 13:48:02 +0300

    update

--- src/utils/multerStorage.ts
+++ src/utils/multerStorage.ts
@@ -1,15 +1,29 @@
-import multer from "multer"
-import path from "path"
+// import multer from "multer"
+// import path from "path"
+// import {put} from "@vercel/blob"
 
-const storage = multer.diskStorage({
-    destination(req, file, callback) {
-        const publicDir = path.resolve(__dirname, "../tmp");
-        callback(null, publicDir);    },
-    filename(req, file, callback) {
-        callback(null, file.originalname)
-    },
-})
+// const storage = multer.diskStorage({
+//     destination(req, file, callback) {
+//         const blob = async()=>{
+//             const blob = await put(file?.name, file, { access: 'public' });
+//             return blob
+
+//         }
+//         // const publicDir = path.resolve(__dirname, "../tmp");
+
+//         callback(null, blob);    },
+//     filename(req, file, callback) {
+//         callback(null, file.originalname)
+//     },
+// })
+
+import multer from "multer";
+import { put } from "@vercel/blob";
+
+// Use memory storage to keep the file in memory
+const storage = multer.memoryStorage();
+
+export const upload = multer({ storage: storage });
 
 
-
-export const upload = multer({storage:storage})
+// export const upload = multer({storage:storage})
