2a762bffa86c1313d7f61d23cace9034e7c588e5
Mark kibocha <mark.varu@student.cuk.ac.ke>
2024-09-23 09:47:40 +0300

    Update tokens.ts

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -4,7 +4,7 @@ const JWT_SECRET = 'your_jwt_secret_key';
 const JWT_REFRESH_SECRET = 'your_refresh_jwt_secret_key';
 
 export const generateAccessToken = (userId: string) => {
-  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '15m' }); // Access token expires in 15 minutes
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '7h' }); // Access token expires in 7hours
 };
 
 export const generateRefreshToken = (userId: string) => {
