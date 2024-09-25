7c99b601185c8a1c0d7f52e802be64a0c915e56b
Mark kibocha <mark.varu@student.cuk.ac.ke>
2024-09-25 11:57:57 +0300

    Update auth.ts

--- src/controllers/auth/auth.ts
+++ src/controllers/auth/auth.ts
@@ -10,7 +10,7 @@ import prisma from "../../utils/prismaConfig";
 // validation schema
 const userSchema = Joi.object({
     firstname: Joi.string().required(),
-    lastname: Joi.string().required(),
+    lastname: Joi.string(),
     phonenumber: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
