a302529a7fc152f2aed6f7d51496adf200ade28d
Mark kibocha <mark.varu@student.cuk.ac.ke>
2024-10-15 18:48:07 +0300

    Update index.ts

--- src/controllers/jobmanagement/index.ts
+++ src/controllers/jobmanagement/index.ts
@@ -771,7 +771,7 @@ const UpdateJobStatusSchema = Joi.object({
   status: Joi.string().valid("CREATED", "ACCEPTED",
     "ASSIGNED",
     "SCHEDULED",
-    "INPROGRESS",
+    "ONGOING",
     "COMPLETED",
     "CANCELLED").required()
 
